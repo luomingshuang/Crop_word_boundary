697ab209fd6032fa6ff9748c7b0ae3d4af7503f8
Mingshuang Luo <37799481+luomingshuang@users.noreply.github.com>
2019-08-19 14:06:57 +0800

    Create crop_word_boundary_video.py

---
+++ crop_word_boundary_video.py
@@ -0,0 +1,18 @@
+def load_features(arr, filename, use_boundary=False, augment=True):
+    """Loads the specified video features.
+    Args:
+        arr (np.array): The loaded numpy feature.
+        filename (str): The name of the mp4 file.
+    Returns:
+        FloatTensor: the visual features of the video as a tensor
+         (length, n_features)"""
+
+    if use_boundary:
+        meta_path = filename.split('.')[0] + '.txt'
+        with open(meta_path, 'r') as f:
+            duration = math.ceil(float(f.readlines()[-1].split(' ')[1]) * 25)
+            st = math.ceil(((30 - duration)/2))
+            ed = math.floor((30 + duration)/2)
+    else:
+        st, ed = 0, 29
+    return arr[st: ed]
